6ddf2bb23f5577d7701d11c29852cf7a6e978698
marely <marely.carcamo@gmail.com>
2024-03-03 03:56:57 -0300

    corregido

--- random-colors.js
+++ random-colors.js
@@ -1,19 +1,23 @@
+// PROGRAMA RANDOM COLOR
+
+// AUTHOR: MARELY
+
+
 /**
- * The JavaScript code generates 150 colored squares with random RGB values when a button is clicked.
- * @param inferior - The `inferior` parameter in the `aleatorio` function represents the lower bound of
- * the range from which a random number will be generated. In this case, it is used to specify the
- * lower limit for generating random RGB values for the color of the squares in the `create_square`
- * function.
- * @param superior - The `superior` parameter in the `aleatorio` function represents the upper limit of
- * the range from which a random number will be generated. In the context of the provided code, it is
- * used to specify the maximum value for the RGB color components (red, green, blue) when creating
- * random
- * @returns The code provided is a JavaScript function that generates a random number within a
- * specified range and creates 150 colored squares with random RGB colors. When the button with the id
- * "bt-color" is clicked, it empties the container with the id "box-square" and calls the
- * `create_square()` function to generate new colored squares.
+ * The JavaScript code generates 150 random colored squares when a button is clicked.
+ * @param inferior - The `inferior` parameter in the `random_color` function represents the lower limit
+ * of the range from which a random color component (red, green, or blue) is generated. In this case,
+ * it is used to specify the minimum value for the RGB color components (0 for minimum intensity).
+ * @param superior - The `superior` parameter in the `random_color` function represents the upper limit
+ * for generating a random color value. In this case, it is used to specify the maximum value for the
+ * RGB color components (red, green, blue) which range from 0 to 255.
+ * @returns The code provided is a JavaScript code snippet that generates 150 square div elements with
+ * random background colors when a button with the id "btn-color" is clicked. The colors are generated
+ * using the `random_color` function which generates random RGB values within the specified range. The
+ * `create_square` function is responsible for creating the square div elements with random colors and
+ * appending them to an element with the id "
  */
-function aleatorio(inferior, superior) {
+function random_color(inferior, superior) {
 	let numPosibilidades = superior - inferior;
 	let aleatorio = Math.random() * numPosibilidades;
 	aleatorio = Math.floor(aleatorio);
@@ -23,9 +27,9 @@ function aleatorio(inferior, superior) {
 function create_square() {
 	let i = 0;
 	while (i < 150) {
-		const r = aleatorio(0, 255);
-		const g = aleatorio(0, 255);
-		const b = aleatorio(0, 255);
+		const r = random_color(0, 255);
+		const g = random_color(0, 255);
+		const b = random_color(0, 255);
 		const colorRGB = `rgb(${r}, ${g}, ${b})`;
 		let square = `<div style="height: 100px; width: 100px; background-color:${colorRGB};"></div>`;
 		$("#box-square").append(square);
